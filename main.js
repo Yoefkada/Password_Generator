0698419db00f1eda7e1ecc45f9ff8f3ac2adf56f
youssef karada <karadayoussef10@gmail.com>
2024-04-23 17:14:20 +0100

    js missing copy

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-function generatePassword() {
-  var length = $("#number").val();
-  var uppercase = $("#uppercase").is(":checked");
-  var lowercase = $("#lowercase").is(":checked");
-  var numbers = $("#numbers").is(":checked");
-  var symbols = $("#symbols").is(":checked");
-
-  var charset = "";
-  if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-  if (numbers) charset += "0123456789";
-  if (symbols) charset += "!@#$%^&*()_+-=";
-
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
-  }
-
-  return password;
-}
+$(document).ready(function() {
+    var password = "P4$5W0rD!";
+    $("#number").on("input", function() {
+      $("span.text-NeonGreen").text($(this).val());
+    });
+    $("#number").trigger("input");
+  
+    function generatePassword() {
+      var length = $("#number").val();
+      var uppercase = $("#uppercase").is(":checked");
+      var lowercase = $("#lowercase").is(":checked");
+      var numbers = $("#numbers").is(":checked");
+      var symbols = $("#symbols").is(":checked");
+    
+      var charset = "";
+      if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      if (lowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+      if (numbers) charset += "0123456789";
+      if (symbols) charset += "!@#$%^&*()_+-=";
+    
+      var password = "";
+      for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+      }
+    
+      return password;
+    }
+  
+    function copyToClipboard() {
+      var passwordInput = $("input[placeholder='P4$5W0rD!']");
+      passwordInput.val(password);
+      passwordInput.select();
+      document.execCommand("copy");
+    }
+  
+    $("button[type=submit]").click(function(e) {
+      e.preventDefault();
+      password = generatePassword();
+      $("input[placeholder='P4$5W0rD!']").val(password);
+    });
+  
+    $("button[type=button]").click(function() {
+      copyToClipboard();
+    });
+  });
   
